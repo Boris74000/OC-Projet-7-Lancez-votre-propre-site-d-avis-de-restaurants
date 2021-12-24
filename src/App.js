23f9705e7424520d0f0985a4f63a94dd2700f8c7
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-12-24 12:14:41 +0100

    Import GoogleMap component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from "react";
+
+import GoogleMap from "./GoogleMap";
+
 function App() {
-  return (
-    <div>
-        <h1>
-            Restaurant Advisor
-        </h1>
-    </div>
-  );
+
+    return (
+        <div>
+            <h1>
+                Restaurant Advisor
+            </h1>
+            <GoogleMap/>
+        </div>
+    );
 }
 
 export default App;
