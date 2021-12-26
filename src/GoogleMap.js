709b362c8732d9b347dc661d7de9f4b36c69dbc8
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2021-12-26 21:37:30 +0100

    Map centered on user position

--- src/GoogleMap.js
+++ src/GoogleMap.js
@@ -2,20 +2,37 @@ import React, {Component} from "react";
 import {Map, GoogleApiWrapper} from 'google-maps-react';
 
 class MapContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude: 45.90610101557426,
+            longitude: 5.116306811445137,
+        };
+    }
+
+    componentDidMount() {
+        navigator.geolocation.watchPosition((position)=>{
+            this.setState({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            })
+        })
+    }
+
     render() {
         return (
             <Map
                 google={this.props.google}
                 style={{width: "500px", height: "500px"}}
                 zoom={10}
-                initialCenter={
+                center={
                     {
-                        lat: 45.90610101557426,
-                        lng: 6.116306811445137
+                        lat: this.state.latitude,
+                        lng: this.state.longitude
                     }
                 }
-
             />
+
         );
     }
 }
