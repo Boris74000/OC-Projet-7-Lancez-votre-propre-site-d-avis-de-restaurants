224a6025605435d0e64d84a25a141b9d223adf41
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-01-20 19:48:02 +0100

    Display average ratings and comments if click

--- src/RestaurantsListItems.js
+++ src/RestaurantsListItems.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 import classes from "./RestaurantsListItems.module.css";
+import RestaurantsListItemsComments from "./RestaurantsListItemsComments";
 
 const RestaurantsListItems = (props) => {
+    const [isCommentShowed, setIsCommentShowed] = useState(false);
+
+    const displayComment = () => {
+        setIsCommentShowed(prevState => !prevState);
+    }
+
     return (
-        <li className={classes.listItem}>
+        <li onClick={displayComment} className={classes.listItem}>
             {props.name}
+            <p>
+                {props.elt.ratings.reduce((previousValue, currentValue) => previousValue + currentValue.stars, 0) / props.elt.ratings.length}
+            </p>
+            {isCommentShowed === true &&
+                props.elt.ratings.map((elt, index) =>
+                    <RestaurantsListItemsComments
+                        key={index}
+                        comment={elt.comment}
+                    />
+                )
+            }
         </li>
     );
 }
