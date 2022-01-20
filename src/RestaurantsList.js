5bd28e0b00dc58d3fea254180370589bc28f9ac4
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-01-20 19:46:07 +0100

    use context

--- src/RestaurantsList.js
+++ src/RestaurantsList.js
@@ -1,19 +1,68 @@
-import React from "react";
-
+import React, {useContext, useState} from "react";
+import {RestaurantContext} from './store/RestaurantContext';
 import RestaurantsListItems from "./RestaurantsListItems";
+import classes from './RestaurantsList.module.css';
+import restaurantsJson from "./restaurants.json";
 
 const RestaurantsList = (props) => {
-    // console.log(props.restaurantsJson);
+    const {restaurants} = useContext(RestaurantContext);
+    const [stateRestaurants, setStateRestaurants] = restaurants;
+    const [isCommentShowed, setIsCommentShowed] = useState(false);
+
+    console.log(restaurants);
+
+    // return dishDataArray.reduce((previousValue, currentValue) => previousValue + currentValue.price * currentValue.amountOrdered, 0).toFixed(2);
+
+    // const displayRatings = () => {
+    //     for (let i = 0; i < stateRestaurants.length; i++) {
+    //         // console.log(stateRestaurants[0].ratings);
+    //         const sum = stateRestaurants[i].ratings.reduce((previousValue, currentValue) => previousValue + currentValue.stars, 0);
+    //         console.log(sum);
+    //     }
+    // }
+    // displayRatings();
+
+    const displayComment = () => {
+        // alert("ici");
+        setIsCommentShowed(prevCheck => !prevCheck);
+        console.log(isCommentShowed);
+    }
 
     return (
-        <ul>
-            {props.restaurantsJson.map((elt, index) =>
+        <ul className={classes.restaurantsList}>
+            { stateRestaurants.map((elt, index) =>
                 <RestaurantsListItems
-                    key={index}
-                    name={elt.restaurantName}
+                    key={ index }
+                    elt = { elt }
+                    name={ elt.restaurantName }
                 />
             )}
         </ul>
+
+        // <ul className={classes.restaurantsList}>
+        //     {stateRestaurants.map((items, index) => {
+        //         return (
+        //             <li key={index} onClick={displayComment} className={classes.restaurantsListItems}>
+        //                 {items.restaurantName}
+        //                 <p>
+        //                     {/*{items.ratings.map((subItems, sIndex) => {*/}
+        //                     {/*    return <li> {subItems.stars} </li>;*/}
+        //                     {/*})}*/}
+        //                     {items.ratings.reduce((previousValue, currentValue) => previousValue + currentValue.stars, 0) / items.ratings.length}
+        //                 </p>
+        //                 {isCommentShowed === true &&
+        //                     <ul>
+        //                         {items.ratings.map((items, index) => {
+        //                             return (
+        //                                 <li>{items.comment}</li>
+        //                             )
+        //                         })}
+        //                     </ul>
+        //                 }
+        //             </li>
+        //         );
+        //     })}
+        // </ul>
     );
 }
 
