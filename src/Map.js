ae9a5546e70ab12541462ae7aa4a98e888a56c78
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-01-20 19:44:44 +0100

    Get bounds and filter restaurants

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,42 @@
-import React, {useState, useEffect} from "react";
-import {GoogleMap, LoadScript, Marker, InfoWindow, useGoogleMap} from '@react-google-maps/api';
-
+import React, { useState, useCallback, useContext } from "react";
+import { GoogleMap, Marker, InfoWindow, useJsApiLoader } from "@react-google-maps/api";
+import { RestaurantContext } from "./store/RestaurantContext";
+import restaurantsJson from "./restaurants.json";
 
 const Map = (props) => {
+    const [map, setMap] = useState(null);
     const [currentPosition, setCurrentPosition] = useState({});
+    const [bounds, setBounds] = useState();
+    // const [markers, setMarkers] = useState(props.restaurantsJson);
+    // const [restaurants, setRestaurants ] = useContext(RestaurantContext);
+
+    // console.log(ctx.restaurants)
+    const {restaurants, restaurantsInBounds} = useContext(RestaurantContext);
+    const [ stateRestaurants, setStateRestaurants ] = restaurants;
+    const [ stateRestaurantsInBounds, setStateRestaurantsInBounds ] = restaurantsInBounds;
+
+    const getBounds = () => {
+        const boundsNordEstlat = map.getBounds().getNorthEast().lat();
+        const boundsNordEstlng = map.getBounds().getNorthEast().lng();
+        const boundsSudOuestlat = map.getBounds().getSouthWest().lat();
+        const boundsSudOuestlng = map.getBounds().getSouthWest().lng();
+
+        const filteredRestaurants = stateRestaurants.filter(restaurant => {
+                if ((restaurant.lat > boundsSudOuestlat) &&
+                    (restaurant.lat < boundsNordEstlat) &&
+                    (restaurant.lng > boundsSudOuestlng) &&
+                    (restaurant.lng < boundsNordEstlng)) {
+                    return true;
+                }
+            }
+        );
+
+        console.log(filteredRestaurants);
+        // setStateRestaurantsInBounds(filteredRestaurants);
+        // console.log(stateRestaurantsInBounds);
+        // console.log(restaurantsInBounds);
+
+    }
 
     const success = position => {
         const currentPosition = {
@@ -12,14 +45,6 @@ const Map = (props) => {
         }
         setCurrentPosition(currentPosition);
     };
-
-    const map = useGoogleMap()
-
-    React.useEffect(() => {
-        if (map) {
-            console.log(map.getBounds());
-        }
-    }, [map])
 
     // useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
@@ -30,62 +55,74 @@ const Map = (props) => {
         height: '400px'
     };
 
-    const getBounds = () => {
+
+    const {isLoaded} = useJsApiLoader({
+        id: "google-map-script",
+        googleMapsApiKey: "AIzaSyC2-n39eQnutXECIDc-9tlNMNFmxzshDtE"
+    });
+
+    const onLoad = useCallback(function callback(map) {
         const bounds = new window.google.maps.LatLngBounds();
-        console.log(bounds);
-    }
+        // map.fitBounds(bounds);
+        setMap(map);
+    }, []);
 
-    return (
-        <LoadScript
-            googleMapsApiKey="AIzaSyC2-n39eQnutXECIDc-9tlNMNFmxzshDtE"
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null);
+    }, []);
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            // center={center}
+            center={currentPosition}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+            // onDragEnd={() => console.log(map.getBounds())}
+            onDragEnd={getBounds}
         >
-            <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={currentPosition}
-                zoom={10}
-                onDragEnd={getBounds}
+            {stateRestaurants.map(
+                (element, index) => <Marker
+                    key={index}
+                    position=
+                        {
+                            {
+                                lat: element.lat,
+                                lng: element.lng
+                            }
+                        }
+
+                    icon={{url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"}}
+                    name={element.restaurantName}
+                />
+            )}
+
+            <Marker
+                position={currentPosition}
             >
-                {props.restaurantsJson.map(
-                    (element, index) => <Marker
-                        key={index}
-                        // ref={onMarkerMounted}
-                        position=
+                <InfoWindow
+                    options=
+                        {
                             {
-                                {
-                                    lat: element.lat,
-                                    lng: element.long
-                                }
+                                pixelOffset:
+                                    {
+                                        width: 0,
+                                        height: -45
+                                    }
                             }
+                        }
+                    position={currentPosition}>
+                    <div>
+                        <p>Your current position</p>
+                    </div>
+                </InfoWindow>
+            </Marker>
 
-                        icon={{url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png"}}
-                        name={element.restaurantName}
-                    />
-                )}
-
-                <Marker
-                    position={currentPosition}
-                >
-                    <InfoWindow
-                        options=
-                            {
-                                {
-                                    pixelOffset:
-                                        {
-                                            width: 0,
-                                            height: -45
-                                        }
-                                }
-                            }
-                        position={currentPosition}>
-                        <div>
-                            <p>Your current position</p>
-                        </div>
-                    </InfoWindow>
-                </Marker>
-            </GoogleMap>
-        </LoadScript>
+        </GoogleMap>
+    ) : (
+        <p>Map can't be load</p>
     );
-};
+}
 
-export default Map;
-
+export default React.memo(Map);
