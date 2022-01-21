5753296d92c16ac461229ce4aa8e172cc4eac38b
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-01-21 18:00:33 +0100

    Creating state restaurantsFilteredInList, updateRestaurantsByRatings in development

--- src/store/RestaurantContext.js
+++ src/store/RestaurantContext.js
@@ -6,10 +6,30 @@ export const RestaurantContext = createContext();
 
 export const RestaurantContextProvider = (props) => {
     const [ restaurants, setRestaurants ] = useState(restaurantsJson);
+    const [ restaurantsFilteredInList, setRestaurantsFilteredInList ] = useState(restaurantsJson);
 
     const updateRestaurants = (filteredRestaurants) => {
         console.log(filteredRestaurants);
-        setRestaurants(filteredRestaurants);
+        setRestaurantsFilteredInList(filteredRestaurants);
+    }
+
+    // En cours de dev
+    const updateRestaurantsByRatings = (minStars, maxStars) => {
+        console.log(minStars, maxStars);
+        // setRestaurantsFilteredInList(filteredRestaurants);
+        for (let i = 0; i < restaurantsFilteredInList.length; i++ ) {
+            console.log(restaurantsFilteredInList[i].ratings);
+
+            for (let j = 0; j < restaurantsFilteredInList[i].ratings.length; j++) {
+                console.log(restaurantsFilteredInList[i].ratings[j]);
+                const restaurantsFilteredByRatings = restaurantsFilteredInList.filter(restaurant => {
+                    if ((restaurant.stars >= minStars) && (restaurant.stars <= maxStars)) {
+                        return true;
+                    }
+                })
+                console.log(restaurantsFilteredByRatings);
+            }
+        }
     }
 
     return (
@@ -17,7 +37,9 @@ export const RestaurantContextProvider = (props) => {
             value={
                 {
                     restaurants: restaurants,
-                    updateRestaurants: updateRestaurants
+                    restaurantsFilteredInList: restaurantsFilteredInList,
+                    updateRestaurants: updateRestaurants,
+                    updateRestaurantsByRatings: updateRestaurantsByRatings
                 }
             }
         >
