9db89f1321c4e15c9fd2161248148d4db4fbc80a
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-01-21 18:02:17 +0100

    Clear code, changing context

--- src/RestaurantsList.js
+++ src/RestaurantsList.js
@@ -1,20 +1,14 @@
-import React, {useContext, useState} from "react";
+import React, {useContext} from "react";
 import {RestaurantContext} from './store/RestaurantContext';
 import RestaurantsListItems from "./RestaurantsListItems";
 import classes from './RestaurantsList.module.css';
 
 const RestaurantsList = () => {
-    const [isCommentShowed, setIsCommentShowed] = useState(false);
     const ctx = useContext(RestaurantContext);
-
-    const displayComment = () => {
-        setIsCommentShowed(prevCheck => !prevCheck);
-        console.log(isCommentShowed);
-    }
 
     return (
         <ul className={classes.restaurantsList}>
-            { ctx.restaurants.map((elt, index) =>
+            { ctx.restaurantsFilteredInList.map((elt, index) =>
                 <RestaurantsListItems
                     key={ index }
                     elt = { elt }
