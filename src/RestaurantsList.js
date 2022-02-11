5288606fb827d4f7c6fd35e24bf55b43c3ef3e81
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-02-11 16:22:30 +0100

    Filtering restaurants based on stars

--- src/RestaurantsList.js
+++ src/RestaurantsList.js
@@ -1,18 +1,31 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {RestaurantContext} from './store/RestaurantContext';
 import RestaurantsListItems from "./RestaurantsListItems";
 import classes from './RestaurantsList.module.css';
 
 const RestaurantsList = () => {
     const ctx = useContext(RestaurantContext);
+    const [restaurantsFilteredWithRatings, setRestaurantsFilteredWithRatings] = useState(ctx.restaurantsFilteredInList);
+
+    useEffect(() => {
+        const ratingsRestaurantsFilter = ctx.restaurantsFilteredInList.filter(elt => {
+            const ratingsAverage = elt.ratings.reduce((previousValue, currentValue) => previousValue + currentValue.stars, 0) / elt.ratings.length;
+            if (ratingsAverage >= ctx.minStars && ratingsAverage <= ctx.maxStars) {
+                return elt;
+            }
+        });
+
+        setRestaurantsFilteredWithRatings(ratingsRestaurantsFilter);
+
+    }, [ctx.minStars, ctx.maxStars]);
 
     return (
         <ul className={classes.restaurantsList}>
-            { ctx.restaurantsFilteredInList.map((elt, index) =>
+            {restaurantsFilteredWithRatings.map((elt, index) =>
                 <RestaurantsListItems
-                    key={ index }
-                    elt = { elt }
-                    name={ elt.restaurantName }
+                    key={index}
+                    elt={elt}
+                    name={elt.restaurantName}
                     lat={elt.lat}
                     lng={elt.lng}
                 />
