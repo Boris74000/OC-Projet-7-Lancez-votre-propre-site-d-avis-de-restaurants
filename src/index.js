a0da369fa9b072e29a7887a013db49d5215570dc
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-02-17 11:02:56 +0100

    Moving RestaurantContextProvider in Index

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import {RestaurantContextProvider} from "./store/RestaurantContext";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <RestaurantContextProvider>
+            <App/>
+        </RestaurantContextProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
