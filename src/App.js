303c37c152691d9bf63f2aef3b73135d78a70d13
Charles-Boris DAUVIER <dauvierboris@hotmail.com>
2022-02-17 11:01:07 +0100

    Adding filterRestaurantsWithBounds, getBounds and addNewRestaurant function. Filter restaurants with ratings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import React, { useState } from "react";
+import React, {useEffect, useState, useContext} from "react";
 
-import Map from "./Map";
-import RestaurantsList from "./RestaurantsList";
-
+import Map from "./components/Map/Map";
+import RestaurantsList from "./components/Restaurants/RestaurantsList";
+import {RestaurantContext} from "./store/RestaurantContext";
 import classes from "./App.module.css";
-import { RestaurantContextProvider } from "./store/RestaurantContext";
-import RestaurantsFilter from "./RestaurantsFilter";
+import RestaurantsFilter from "./components/Restaurants/RestaurantsFilter";
+import restaurantsJson from "./assets/restaurants.json";
 
 function App() {
+    const ctx = useContext(RestaurantContext);
+    const [restaurantsFiltered, setRestaurantsFiltered] = useState(restaurantsJson);
+
+    const addNewRestaurant = (data) => {
+        setRestaurantsFiltered([...restaurantsFiltered, data]);
+    };
+
+    const getBounds = (boundsNordEstlat, boundsNordEstlng, boundsSudOuestlat, boundsSudOuestlng) => {
+        filterRestaurantsWithBounds(boundsNordEstlat, boundsNordEstlng, boundsSudOuestlat, boundsSudOuestlng);
+    };
+
+    const filterRestaurantsWithBounds = (boundsNordEstlat, boundsNordEstlng, boundsSudOuestlat, boundsSudOuestlng) => {
+        const filteredRestaurants = ctx.restaurants.filter(restaurant => {
+                if ((restaurant.lat > boundsSudOuestlat) &&
+                    (restaurant.lat < boundsNordEstlat) &&
+                    (restaurant.lng > boundsSudOuestlng) &&
+                    (restaurant.lng < boundsNordEstlng)) {
+                    return true;
+                }
+            }
+        );
+        setRestaurantsFiltered(filteredRestaurants);
+    };
+
+    useEffect(() => {
+        const ratingsRestaurantsFilter = ctx.restaurants.filter(elt => {
+            const ratingsAverage = elt.ratings.reduce((previousValue, currentValue) => previousValue + currentValue.stars, 0) / elt.ratings.length;
+            if (ratingsAverage >= ctx.minStars && ratingsAverage <= ctx.maxStars) {
+                return elt;
+            }
+        });
+        if (ratingsRestaurantsFilter.length > 0) {
+            setRestaurantsFiltered(ratingsRestaurantsFilter);
+        }
+
+    }, [ctx.minStars, ctx.maxStars]);
+
+
     return (
-        <RestaurantContextProvider>
+        <>
             <section>
                 <RestaurantsFilter/>
             </section>
-            <section className={ classes.mapAndListContainer }>
+            <section className={classes.mapAndListContainer}>
                 <RestaurantsList
-                    className={ classes.list }
+                    className={classes.list}
+                    restaurantsFiltered={restaurantsFiltered}
                 />
-                <Map/>
+                <Map
+                    getBounds={getBounds}
+                    restaurantsFiltered={restaurantsFiltered}
+                    addNewRestaurant={addNewRestaurant}
+                />
             </section>
-        </RestaurantContextProvider>
+        </>
     );
 }
 
